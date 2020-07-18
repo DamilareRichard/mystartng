702d6715a9f0ff2f424013218800fe7f763b7d6f
Damilare Richard Olagbende <damilareshinaayo@gmail.com>
2020-07-18 11:35:46 +0100

    Start

---
+++ FAQ.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('navLinks');
+
+
+hamburger.addEventListener('click', () => {
+	navUL.classList.toggle('show');
+
+
+
+
+});
+
